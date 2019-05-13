00cac1cd135e2dc8aca5c4294c2d3bda156f98f7
Jack Grzechowiak <jgrzechowiak@rite-solutions.com>
2019-05-13 08:09:45 -0400

    Updated stuff

--- src/minigames/MiniGame.js
+++ src/minigames/MiniGame.js
@@ -60,13 +60,13 @@ export default class MiniGame extends Phaser.Scene {
      * The horizontal position of the minigame window
      * @type {number}
      */
-    this.x = 25;
+    this.x = 0;
     
     /**
      * The vertical position of the minigame window
      * @type {number}
      */
-    this.y = 25;
+    this.y = 0;
     
     /**
      * The difficulty of the minigame
@@ -128,162 +128,5 @@ export default class MiniGame extends Phaser.Scene {
     this.h = this.sys.game.config.height;
 
     const background = this.add.graphics();
-    background.fillStyle( 0x999999, 1 );
-    background.fillRect( this.x, this.y, this.w - 50, this.h - 50 );
-
-    this.add.text( this.x + 16, this.y + 16, this.name,
-      {
-        fontSize: '24px',
-        fill: '#FFF'
-      } );
-    this.add.text( this.x + 16, this.y + 40, this.description,
-      {
-        fontSize: '12px',
-        fill: '#FFF'
-      } );
-
-    this.alertText = this.add.text( this.w / 2, this.h / 2, this.text,
-      {
-        fontSize: '40px',
-        fill: '#FFF'
-      } );
-    this.alertText.setPosition(
-      this.alertText.x - this.alertText.width / 2,
-      this.alertText.y - this.alertText.height / 2
-    );
-
-    // Show text for 1 second, then start mini game
-    this.startTimeout = setTimeout( () => {
-      this.startGame();
-    }, 1000 );
-
-    this.timerBar = this.add.graphics();
-    this.timerBar.fillStyle( 0xFFFFFF, 0.4 );
-    this.timerBar.fillRect( this.x, this.h - 10 - this.y, this.w - 50, 10 );
-
-    /**
-     * Object used to store the keys available for use in the minigame
-     * @type {Object}
-     */
-    this.keys = KeyBinding.createKeys( this, [ 'interact' ] );
-  }
-
-  /**
-   * Updates every tick of the game loop
-   * @param {number} time The current time
-   * @param {number} delta The delta time in ms since the last frame
-   */
-  update( time, delta ) {
-    if ( this.started ) {
-      if ( this.duration !== -1 ) {
-        if ( this.elapsedTime > this.duration ) {
-          this.lose();
-        }
-        this.elapsedTime += delta * this.timeScale;
-
-        this.timerBar.clear();
-        this.timerBar.fillStyle( 0xFFFFFF, 0.4 );
-        this.timerBar.fillRect( this.x,
-          this.h - 10 - this.y,
-          this.w - ( ( this.w - 50 ) * ( this.elapsedTime / this.duration ) )
-          - 50,
-          10
-        );
-      }
-      this.updateMiniGame( time, delta );
-    }
-    else if ( this.keys.interact.isDown ) {
-      this.openInventory();
-    }
-  }
-
-  /**
-   * Opens the inventory
-   */
-  openInventory() {
-    clearTimeout( this.startTimeout );
-    this.input.keyboard.resetKeys();
-    this.scene
-      .launch( 'InventoryScene', {
-        parent: this,
-        inventory: this.parent.scene.player.inventory,
-        context: ItemType.MINIGAME
-      } )
-      .bringToTop( 'InventoryScene' )
-      .pause();
-  }
-
-  /**
-   * Closes the inventory
-   * @param {Item} item The item selected from the inventory
-   */
-  closeInventory( item ) {
-    this.scene.stop( 'InventoryScene' );
-    this.input.keyboard.resetKeys();
-    this.scene.resume();
-
-    if ( item && item.itemType === ItemType.MINIGAME ) {
-      item.use( { minigame: this } );
-      this.parent.scene.player.inventory.items.splice( item.inventoryIndex, 1 );
-      this.startGame();
-    }
-    else {
-      this.startTimeout = setTimeout( () => {
-        this.startGame();
-      }, 1000 );
-    }
-  }
-
-  /**
-   * Called right before the mini game starts
-   */
-  startGame() {
-    this.alertText.destroy();
-    this.started = true;
-  }
-
-  /**
-   * This method will only be updated once the mini game has officially started.
-   * The mini game will start after the alert text disappears.
-   * @param {number} time The current time
-   * @param {number} delta The delta time in ms since the last frame
-   */
-  updateMiniGame( time, delta ) {
-    // NOTE: Override this in mini game classes
-    throw new Error( 'updateMiniGame() must be overriden' );
-  }
-
-  /**
-   * Called when the mini game has been won. Will redirect back to combat.
-   */
-  win() {
-    const timeLeft = this.duration - this.elapsedTime;
-
-    this.reset();
-    this.parent.continueCombat( {
-      win: true,
-      damage: Math.ceil( timeLeft / 1000 ) * this.difficulty
-    } );
-  }
-
-  /**
-   * Called when the mini game has been lost. Will redirect back to combat.
-   */
-  lose() {
-    this.reset();
-    this.parent.continueCombat( {
-      win: false,
-      damage: 0
-    } );
-  }
-
-  /**
-   * Resets the mini game to its original state to be played again.
-   */
-  reset() {
-    this.timerBar.destroy();
-    this.elapsedTime = 0;
-    this.started = false;
-    this.timeScale = 1;
-  }
-}
+    background.fillStyle( 0x333333, 0.9 );
+    background.fillRect( this.x, this.y, this.w, this.h );
